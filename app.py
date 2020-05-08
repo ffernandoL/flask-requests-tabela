63f1160fc350d3b95018741c3005fb5eab8db579
Fernando Luis <fernnand.luis@gmail.com>
2020-05-07 21:20:14 -0300

    Request de um produto

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+produtos = [
+    ['Refrigerante',4.50],
+    ['Pizza',2.50],
+    ['Suco',24.90],
+    ['Salgado',5.50],
+    ['Lanche',18.50]
+]
+
+@app.route('/')
+def index():    
+
+    return render_template(
+        'index.html',
+        titulo='Table',
+        produtos=produtos                     
+    )
+
+@app.route('/prod/<int:id>')
+def produto(id):
+    prod = produtos[id]
+    return render_template(
+        'produtos.html',
+        produto=prod
+    )
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/store', methods=['POST'])
+def store():
+    nome = request.form['nome']
+    preco = float(request.form['preco'])
+    produtos.append([nome, preco])
+    
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
